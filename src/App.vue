c4f569de366c5e3c1d093fa92c47f7fe69acc8ba
FrancescoCina <francesco.cina04@gmail.com>
2021-09-15 11:17:30 +0200

    Create componenti Header, Main e Footer. Collegato style.scss ad App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
-  <div id="app">First Commit</div>
+  <div id="app">
+    <Header></Header>
+    <Main></Main>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
+import Header from "@/components/Header.vue";
+import Main from "@/components/Main.vue";
+import Footer from "@/components/Footer.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Header,
+    Main,
+    Footer,
+  },
 };
 </script>
 
 <style lang="scss">
+@import "assets/scss/style.scss";
 </style>
